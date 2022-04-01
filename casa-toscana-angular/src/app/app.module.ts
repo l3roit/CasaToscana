dac8c029de4ae25a787c24ce15ada4e4bee5d42f
Jonas Scherer <scherer.jonas22@gmail.com>
2022-04-01 12:36:52 +0200

    Break

--- casa-toscana-angular/src/app/app.module.ts
+++ casa-toscana-angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
